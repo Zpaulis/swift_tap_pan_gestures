1d1d7bcb9256c9378ef95fc3f1ca178d201a8728
Paulis Zabarovskis <paulis@zabarovski.lv>
2022-06-09 18:12:31 +0300

    Initial Commit

---
+++ swift_tap_pan_gestures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_tap_pan_gestures
+//
+//  Created by Paulis Zabarovskis on 09/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
